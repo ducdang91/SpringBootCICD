ad8e08f537632ba5e9567df8ac42277c25187ebd
Duc Dang <dtduc1991@gmail.com>
2022-10-24 15:05:11 +0700

    first commit

---
+++ src/main/java/com/ea/SpringBasic/libraries/WebDriverLibrary.java
@@ -0,0 +1,35 @@
+package com.ea.SpringBasic.libraries;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class WebDriverLibrary {
+
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
+    public WebDriver getChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    @Scope("driverscope")
+    public WebDriver getFirefoxDriver() {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver(firefoxOptions);
+    }
+
+
+}
